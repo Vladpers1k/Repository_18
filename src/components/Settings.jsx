bca099f6631bf3fa9fd1cac57aa1c3a3011c1f94
Vladpers1k <vladik340123@gmail.com>
2025-01-16 11:08:21 +0200

    major change for users.jsx and not big update for steetings.jsx

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,15 +6,11 @@ const Settings = () => {
   const dispatch = useDispatch()
   const theme = useSelector((state) => state.settings.theme)
 
-  const handleToggleTheme = () => {
-    dispatch(toggleTheme())
-  }
-
   return (
     <div>
       <h2>Settings Page</h2>
       <p>Current Theme: {theme}</p>
-      <button onClick={handleToggleTheme}>Toggle Theme</button>
+      <button onClick={() => dispatch(toggleTheme())}>Toggle Theme</button>
     </div>
   )
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import UserList from './Nested/UserList'
+import { addUser, removeUser } from '../store/usersSlice'
 
 const Users = () => {
   const users = useSelector((state) => state.users)
+  const dispatch = useDispatch()
+  const [newUser, setNewUser] = useState('')
+
+  const handleAddUser = () => {
+    if (newUser) {
+      dispatch(addUser({ id: Date.now(), name: newUser }))
+      setNewUser('')
+    }
+  }
+
+  const handleRemoveUser = (id) => {
+    dispatch(removeUser(id))
+  }
 
   return (
     <div className="users-page">
       <h2>Users Page</h2>
       <UserList users={users} />
+      <input type="text" value={newUser} onChange={(e) => setNewUser(e.target.value)} placeholder="Add new user" />
+      <button onClick={handleAddUser}>Add User</button>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} <button onClick={() => handleRemoveUser(user.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
